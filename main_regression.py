289fad4d5361a7793ecd2ada38c89973fdf7f925
ThierryBleau <thierryb1996@gmail.com>
2019-01-25 18:25:42 -0500

    closed form function

--- main_regression.py
+++ main_regression.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import data_preparation
 
 raw = data_preparation.main()
+
 def get_y_vector(data):
     y = []
     for i in range(len(data)):
@@ -12,8 +13,9 @@ def get_y_vector(data):
 y = np.array(get_y_vector(raw))
 print(y)
 print(raw[-1])
-ones = np.ones(len(raw))
-x = np.column_stack((ones,raw))
-w = np.dot(np.linalg.pinv(x),y)
 
-regression = w[0] + w[1]*raw
+def closed_form():
+    x = np.column_stack((ones,raw))
+    w = np.dot(np.linalg.pinv(x),y)
+    regression = w[0] + w[1]*raw
+    return(regression)
